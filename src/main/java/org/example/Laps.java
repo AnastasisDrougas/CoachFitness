42e419be1402adb892463a2ea46473f4bff8b3ed
angjelo <angjelohoxhaj2022@gmail.com>
2025-12-07 18:33:54 +0200

    v1.3

--- src/main/java/org/example/Laps.java
+++ src/main/java/org/example/Laps.java
@@ -1,9 +1,17 @@
 package org.example;
 
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
 import java.util.ArrayList;
 
 public class Laps {
     ArrayList<Tracks> tracks = new ArrayList<>();
 
+    public Laps(Node node) {
+
+        ArrayListConverter<Laps> converter = new ArrayListConverter<>(activityElement.getElementsByTagName("Tracks"),Lapnode -> new Laps(Lapnode));
+        tracks = converter.getList();
+    }
 
 }
